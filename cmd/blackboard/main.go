a81f6e0b07b562fad5e52929b6e5a40aff52e7cb
Keydose <michaelkelly@myself.com>
2022-04-22 18:30:13 +0100

    Ver bump

--- cmd/blackboard/main.go
+++ cmd/blackboard/main.go
@@ -184,7 +184,7 @@ func Wipe() {
 // https://semver.org/
 func main() {
 	commando.SetExecutableName("bb").
-		SetVersion("v0.3.3").
+		SetVersion("v0.3.4").
 		SetDescription("Using text files under the hood, Blackboard aims to be a minimalistic task management app that focuses on what feels natural.")
 
 	commando.Register("list").
